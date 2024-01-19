d5eaf0233f3ab83f7a353cfce75bb82d4d2bd97f
gschram <giovani.schram@digio.com.br>
2024-01-19 09:58:42 -0300

    Refactor manufacture dynamodb table.

--- scripts/05_create_dynamodb.sh
+++ scripts/05_create_dynamodb.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
 echo "########### Creating dynamodb tables ###########"
-aws --endpoint-url=http://localhost:4566 --profile localstack dynamodb create-table --table-name MANUFACTURE_PURCHASE --key-schema AttributeName=id,KeyType=HASH --attribute-definitions AttributeName=id,AttributeType=S --billing-mode PAY_PER_REQUEST --region us-east-1
-aws --endpoint-url=http://localhost:4566 --profile localstack dynamodb update-time-to-live --table-name MANUFACTURE_PURCHASE --time-to-live-specification Enabled=true,AttributeName=ttl
+aws --endpoint-url=http://localhost:4566 --profile localstack dynamodb create-table \
+--table-name manufacture_purchase_entity \
+--attribute-definitions AttributeName=id,AttributeType=S AttributeName=status,AttributeType=S AttributeName=date,AttributeType=S \
+--key-schema AttributeName=id,KeyType=HASH \
+--billing-mode PAY_PER_REQUEST \
+--region us-east-1 \
+--global-secondary-indexes \
+    "[
+        {
+            \"IndexName\": \"purchase-status-date-index\",
+            \"KeySchema\": [{\"AttributeName\":\"status\",\"KeyType\":\"HASH\"},
+                            {\"AttributeName\":\"date\",\"KeyType\":\"RANGE\"}],
+            \"Projection\":{
+                \"ProjectionType\":\"ALL\"
+            }
+        }
+    ]"
+
+aws --endpoint-url=http://localhost:4566 --profile localstack dynamodb update-time-to-live \
+--table-name manufacture_purchase_entity \
+--time-to-live-specification Enabled=true,AttributeName=ttl
