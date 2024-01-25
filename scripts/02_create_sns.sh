a60cb0052d429ce510f67e572a4d97bf13351f3a
Antonio Miranda <antoniogabrielti@gmail.com>
2024-01-25 14:52:40 -0300

    config: criando novo topico SNS e fila SQS para postagem quando criar um pagamento

--- scripts/02_create_sns.sh
+++ scripts/02_create_sns.sh
@@ -4,4 +4,5 @@ echo "########### Creating sns topics ###########"
 aws --endpoint-url=http://localhost:4566 sns create-topic --profile=localstack --name local-cart-closed-topic
 aws --endpoint-url=http://localhost:4566 sns create-topic --profile=localstack --name local-payment-done-topic
 aws --endpoint-url=http://localhost:4566 sns create-topic --profile=localstack --name local-purchase-created-topic
+aws --endpoint-url=http://localhost:4566 sns create-topic --profile=localstack --name local-payment-created-topic
 
--- scripts/03_create_sqs.sh
+++ scripts/03_create_sqs.sh
@@ -4,3 +4,4 @@ echo "########### Creating sqs queues ###########"
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-payment-cart-closed-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-purchase-payment-done-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-manufacture-purchase-created-queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-payment-created-queue
