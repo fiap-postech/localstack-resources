530a66db4948198173865e7930184aca7539734c
Eberson Oliveira <eberson.olivera@gmail.com>
2024-02-17 17:13:36 -0300

    added queues and topics to be compliant with proposed saga pattern

--- scripts/03_create_sqs.sh
+++ scripts/03_create_sqs.sh
@@ -2,6 +2,9 @@
 
 echo "########### Creating sqs queues ###########"
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-payment-cart-closed-queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-payment-purchase-created-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-purchase-payment-done-queue
-aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-manufacture-purchase-created-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-payment-created-queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-notification-purchase-status-queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --profile=localstack --queue-name local-manufacture-purchase-paid-queue
+
--- scripts/04_create_sns_subscriptions.sh
+++ scripts/04_create_sns_subscriptions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "########### Creating sns subscriptions ###########"
-aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-cart-closed-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-payment-cart-closed-queue --attributes RawMessageDelivery=true
+aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-cart-closed-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-purchase-cart-closed-queue --attributes RawMessageDelivery=true
+aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-purchase-created-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-payment-purchase-created-queue --attributes RawMessageDelivery=true
 aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-payment-done-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-purchase-payment-done-queue --attributes RawMessageDelivery=true
-aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-purchase-created-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-manufacture-purchase-created-queue --attributes RawMessageDelivery=true
 aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-payment-created-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-payment-created-queue --attributes RawMessageDelivery=true
+aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-purchase-status-notification-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-notification-purchase-status-queue --attributes RawMessageDelivery=true
+aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-purchase-paid-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-manufacture-purchase-paid-queue --attributes RawMessageDelivery=true
+
 
